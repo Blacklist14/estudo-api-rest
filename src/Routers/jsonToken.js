4095be8ca43623ffef7a2e052154360f4df74003
Blacklist14 <luis.arigato@hotmail.com>
2022-03-10 18:20:58 -0300

    Corrigindo JWT

--- src/Routers/jsonToken.js
+++ src/Routers/jsonToken.js
@@ -5,14 +5,10 @@ const jwt = require("jsonwebtoken")
 function verifyJWT (req,res,next){
     const token = req.headers.token
     if(!token){return res.status(401).send({auth:false,message:"No token provided"})}
-    const decode = jwt.verify(token,process.env.SECRET,(err,decoded)=>{
+    jwt.verify(token,process.env.SECRET,(err,decoded)=>{
         if(err){return res.status(500).json({ auth: false, message: 'Failed to authenticate token.' });}
-
+        next()
     })
-    if(decode){
-        return decode
-    }
-    next()
 }
 
 module.exports = verifyJWT
